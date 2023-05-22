58c09e5d94197470cb1886eeb4970a447ba9c99f
othea32 <othea32@gmail.com>
2023-05-22 12:53:35 -0700

    data types

---
+++ js/app.js
@@ -0,0 +1,70 @@
+console.log("hello world");
+
+console.log("object");
+
+let solu = 10 % 3;
+console.log(solu);
+
+// newNumber = 75;
+// // newNumber = newNumber + 1;
+// // newNumber -= 1;
+// newNumber--;
+
+// console.log(newNumber);
+
+// let newNumber = 9;
+// let currentNumber = newNumber++;
+// console.log('current ' + currentNumber);
+
+// console.log(4 < 2 || 2===3);
+
+// console.log("hello how are" + " you" + " doing" + " today?");
+
+const a = "a";
+const b = "b";
+
+console.log(a < b); // true
+
+// const frost =
+// 	"Nature’s first green is gold, " +
+// 	"Her hardest hue to hold. " +
+// 	"Her early leaf’s a flower; " +
+// 	"But only so an hour. " +
+// 	"Then leaf subsides to leaf. " +
+// 	"So Eden sank to grief, " +
+// 	"So dawn goes down to day. " +
+// 	"Nothing gold can stay. ";
+
+// console.log(frost)
+
+// const frost =
+// 	"Nature’s first green is gold, \
+// Her hardest hue to hold. \
+// Her early leaf’s a flower; \
+// But only so an hour. \
+// Then leaf subsides to leaf. \
+// So Eden sank to grief, \
+// So dawn goes down to day. \
+// Nothing gold can stay.";
+
+// console.log(frost);
+
+// console.log('They said, "this string does not need an escape!"');
+
+// const frost =
+// 	"Nature’s first green is gold, \n\
+// Her hardest hue to hold. \n\
+// Her early leaf’s a flower; \n\
+// But only so an hour. \n\
+// Then leaf subsides to leaf. \n\
+// So Eden sank to grief, \n\
+// So dawn goes down to day. \n\
+// Nothing gold can stay.";
+
+// console.log(frost);
+
+let person = "john";
+
+let sentence = "hey how are you";
+
+console.log(`${sentence} ${person}`);
